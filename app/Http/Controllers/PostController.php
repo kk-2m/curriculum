073a8695e58dc63fb9401596c05aa5b56ff5e93a
koki-HT <kishi.koki6@gmail.com>
2023-08-09 17:29:20 +0900

    blogデータのHTML的表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,6 +9,11 @@ class PostController extends Controller
 {
 	public function index(Post $post)
 	{
-		return $post->get();
+		// viewインスタンスのwithメソッドを使い、viewに値を渡す
+		// blade内で使う変数を'posts'と設定
+		// 'posts'の中身にgetを使い、インスタンス化した$postを代入
+		return view('posts.index')->with(['posts' => $post->get()]);
+		
 	}
 }
+?>
