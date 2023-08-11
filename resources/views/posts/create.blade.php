0dbc31f40b7800f08ec6a83bb54a060986ce0911
koki-HT <kishi.koki6@gmail.com>
2023-08-11 16:23:17 +0900

    add 投稿記事作成画面

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,20 +8,24 @@
 		<!-- width=device-width : 表示領域の幅をPCやスマホ等の端末画面の幅に合わせられる -->
 		<!-- initial-scale=1.0 : 初期のズーム倍率が1.0倍になる -->
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<title>blog</title>
+		<title>Blog</title>
 	</head>
 	<body class="antialiased">
-		<h1 class='title'>
-			{{ $post->title }}
-		</h1>
-		<div class='content'>
-			<div class='content_post'>
-				<h3>本文</h3>
-				<p class='body'>{{ $post->body }}</p>
+		<form action="/posts" method="POST">
+			<!-- 他のサイトからのリクエストを許容しない -->
+			@csrf
+			<div class="title">
+				<h2>title</h2>
+				<input type="text" name=post[title] placeholder="タイトル">
 			</div>
-		</div>
+			<div class="body">
+				<h2>Body</h2>
+				<textarea name="post[body]" placeholder="今日も１日お疲れ様でした。"></textarea>
+			</div>
+			<input type="submit" value="store">
+		</form>
 		<div class='footer'>
-			<a href="/">back</a>
+			<a href="/">[back]</a>
 		</div>
-	<body>
+	</body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,7 @@ use App\Http\Controllers\PostController;
 
 Route::get('/', [PostController::class, 'index']);
 
+Route::get('/posts/create', [PostController::class, 'create']);
+
 // 暗黙の結合によって{post}にはPostControllerのshowメソッドの引数のpostのidが格納される
 Route::get('posts/{post}', [PostController::class, 'show']);
